af43e10159d7f96681c67978cd8b667067f997f4
damontana7 <49513168+damontana7@users.noreply.github.com>
2019-06-29 14:43:41 +1000

    Add files via upload

---
+++ gal2.js
@@ -0,0 +1,1 @@
+<script type="text/javascript">(function($){$(document).ready(function(){$('.popup-youtube').magnificPopup({disableOn:100,type:'iframe',mainClass:'mfp-fade',removalDelay:160,preloader:false,fixedContentPos:false});});})(jQuery)</script>
